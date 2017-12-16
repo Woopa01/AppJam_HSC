6e141c05f6513f568ec3fb015629f5d21f677062
alal8272 <alal8272@gmail.com>
2017-12-17 04:16:29 +0900

    add splashscreen

--- app/src/main/java/com/example/dsm2017/appjam_hsc/LoginActivity.java
+++ app/src/main/java/com/example/dsm2017/appjam_hsc/LoginActivity.java
@@ -31,6 +31,7 @@ public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        startActivity(new Intent(this, SplashActivity.class));
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_login);
 
---
+++ app/src/main/java/com/example/dsm2017/appjam_hsc/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.example.dsm2017.appjam_hsc;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by dsm2017 on 2017-12-17.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Intent intent = new Intent(SplashActivity.this,LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
