dede8b401d50f4a1251656f1dc9fcb50c6c45b31
Woopa01 <woopa012@gmail.com>
2017-12-16 20:01:17 +0900

    commit

--- app/src/main/java/com/example/dsm2017/appjam_hsc/MainActivity.java
+++ app/src/main/java/com/example/dsm2017/appjam_hsc/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.layout_login);
 
         signin_id = (EditText) findViewById(R.id.EditText_id);
         signin_password = (EditText) findViewById(R.id.EditText_password);
